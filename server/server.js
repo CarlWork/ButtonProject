4a60c236f93dafaed87fe02b0d173b3aaaa28fc9
CarlWork <carllworks@gmail.com>
2023-03-02 12:38:37 -0600

    RollbarImplementation

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '6fc82eb995c5494a84576e98588c42f9',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 
 const app = express()
 
@@ -9,6 +20,7 @@ app.use(cors())
 app.use(express.static('public'))
 
 app.get('/', (req,res)=> {
+    rollbar.log('site visited')
     res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
 })
 app.get('/css', (req,res) => {
@@ -18,7 +30,13 @@ app.get('/js', (req,res)=> {
     res.status(200).sendFile(path.join(__dirname, '../public/main.js'))
 
 })
-
+app.get('/', (req, res) => {
+    try {
+        nonExistentFunction()
+    } catch (error) {
+        rollbar.error(err, req)
+    }
+})
 
 
 app.listen(4000, console.log('App Running on 4004'))
